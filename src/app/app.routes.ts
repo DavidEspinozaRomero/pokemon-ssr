2fd59f4d39c042f1a0319f949ea25a23d21cd5e1
David E <deerhou@gmail.com>
2025-03-14 18:26:31 -0500

    upgrade (app) ssr to the app and meta tags

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'about', loadComponent: () => import("./pages/about/about-page.component") },
-    { path: 'contact', loadComponent: () => import("./pages/contact/contact-page.component") },
-    { path: 'pricing', loadComponent: () => import("./pages/pricing/pricing-page.component") },
+    { path: 'about', title: 'About page', loadComponent: () => import("./pages/about/about-page.component") },
+    { path: 'contact', title: 'Contact page', loadComponent: () => import("./pages/contact/contact-page.component") },
+    { path: 'pricing', title: 'Pricing page', loadComponent: () => import("./pages/pricing/pricing-page.component") },
     { path: '**', redirectTo: 'about' },
 ];
--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-page',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
 })
-export default class AboutPageComponent {
+export default class AboutPageComponent implements OnInit {
+  title = inject(Title)
+  meta = inject(Meta)
+  platform = inject(PLATFORM_ID)
+
+  ngOnInit(): void {
+    this.title.setTitle('About')
+    this.meta.addTag({ name: 'description', content: 'Este es mi about page' })
+
+    if (!isPlatformServer(this.platform)) {
+      document.title = 'about page'
+    }
+  }
 
 }
