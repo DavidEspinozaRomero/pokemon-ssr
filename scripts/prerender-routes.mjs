fc1b709963e0a68b4cd8c2dfb0118cf8cd65f81d
David E <deerhou@gmail.com>
2025-03-19 18:08:16 -0500

    feat routes function to create file of routes.txt

---
+++ scripts/prerender-routes.mjs
@@ -0,0 +1,24 @@
+import { writeFile, writeFileSync } from "node:fs";
+// import { join, dirname } from "node:path";
+const FILENAME = 'routes.txt';
+const TOTALPOKEMONS = 10;
+const TOTALPAGES = 5;
+
+(async () => {
+
+    const pokemonIds = Array.from({ length: TOTALPOKEMONS }, (_, i) => i + 1)
+    const numberOfPages = Array.from({ length: TOTALPAGES }, (_, i) => i + 1)
+
+    let fileContent = pokemonIds.map(id => '/pokemons/' + id).join('\n')
+    fileContent += '\n'+ numberOfPages.map(page => '/pokemons/page/' + page).join('\n')
+
+    console.log(fileContent);
+    // todo: llamar a la api
+    // https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
+    // const call = await fetch('https://pokeapi.co/api/v2/pokemon')
+    // let resp = await call.json()
+
+    // crear el archivo
+    writeFileSync(FILENAME, fileContent)
+})()
+
