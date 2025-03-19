0cbe6cecdfb5ddb744de969b17abecb8dd21fd57
David E <deerhou@gmail.com>
2025-03-19 16:07:10 -0500

    feat home-page adding a home page to the app and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: '', title: 'Home page', loadComponent: () => import("./pages/home/home-page.component") },
     { path: 'about', title: 'About page', loadComponent: () => import("./pages/about/about-page.component") },
     { path: 'contact', title: 'Contact page', loadComponent: () => import("./pages/contact/contact-page.component") },
     { path: 'pricing', title: 'Pricing page', loadComponent: () => import("./pages/pricing/pricing-page.component") },
